65eace92895cffeaf9b4faf2d14c0b502f2befcf
Nazanin Niayesh <e11809924@student.tuwien.ac.at>
2021-12-07 13:56:50 +0100

    form entity defined

---
+++ hackathon-calc/src/app/entity/form.ts
@@ -0,0 +1,12 @@
+
+export class form {
+  id?: number;
+  income?: number;
+  category?: string;
+  children?: number;
+  loneParent?: boolean;
+  commute?: string;
+  commuteDist?: number;
+  fabo17?: number;
+  fabo18?: number;
+}
