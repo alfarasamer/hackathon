621365d7233161bf316ff4a61ccb6f48cc981049
Oskar Lang <84227919+oskiii69@users.noreply.github.com>
2021-12-07 15:56:06 +0100

    begin work on form

--- hackathon-calc/src/app/components/form/form.component.ts
+++ hackathon-calc/src/app/components/form/form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-form',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
-  constructor() { }
+  inputForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
+    this.inputForm = this.formBuilder.group({
+      income: [0, [Validators.required, Validators.min(0)]],
+      children: [0, [Validators.required, Validators.min(0)]],
+      loneParent: [false, []],
+      commute: ['klein', []],
+      commuteDistance: [0, [Validators.min(0)]],
+      fabo17: [0, [Validators.min(0)]],
+      fabo18: [0, [Validators.min(0)]]
+    })
+  }
 
   ngOnInit(): void {
   }
 
+  onSubmit(): void {
+
+  }
 }
